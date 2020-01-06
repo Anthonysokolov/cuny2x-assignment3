314b6a3430e311709ce8f9a93bc905fcdb31c846
Anthony <anthony@Apples-MacBook-Pro.local>
2020-01-06 13:41:04 -0500

    first three

---
+++ script.js
@@ -0,0 +1,19 @@
+// Select section with id of container without using query selector
+document.getElementById("container")
+// Select section with id of container using query selector
+console.log(document.querySelector("#container"))
+
+// Select all of the list items with a class of second
+console.log(document.getElementsByClassName("second"))
+
+// Select a list item with a class of third, but only the list item inside of the ol tag
+//console.log(document.getElementsByTagName("ol").getElementsByClassName("third"))
+
+var content  = document.querySelector(".ol")
+// Give the section with an id of container the text Hello
+
+
+// Add the class main to the div with a class of footer
+
+
+// Remove the class main on the div with a class of footer
